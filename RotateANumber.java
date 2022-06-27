edc31b19f1ebbd3db2b832f6ac9fb576eae950d5
Rahul Prasad <rahul.prsd.in@gmail.com>
2022-06-28 01:09:32 +0530

    Update RotateANumber.java

--- RotateANumber.java
+++ RotateANumber.java
@@ -1,3 +1,4 @@
+// SOLUTION 1: Converting Number to String - SLOWER 
 import java.util.*;
    
    public class Main{
@@ -18,3 +19,29 @@ import java.util.*;
       System.out.println(Integer.parseInt(res));
     }
    }
+// SOLUTION 2: Using Number - FASTER 
+// import java.util.*;
+   
+//    public class Main{
+   
+//    public static void main(String[] args) {
+//      Scanner scn = new Scanner(System.in);
+//      int num = scn.nextInt();
+//      int rotation = scn.nextInt();
+//      int digitCount = 0;
+//      int temp = num;
+//      while(temp!=0){
+//        temp = temp/10;
+//        digitCount++;
+//      }
+//      rotation = rotation%digitCount;
+//      if(rotation<0){
+//        rotation = digitCount+rotation;
+//      }
+//      int div = (int)Math.pow(10, rotation);
+//      int firstHalf = num/div;
+//      int secondHalf = num%div;
+//      secondHalf = secondHalf*(int)Math.pow(10, digitCount-rotation);
+//      System.out.println(secondHalf+firstHalf);
+//     }
+//    }
