af1ad114377d03d6017ed76aa197b69520b42b39
Rahul Prasad <rahul.prsd.in@gmail.com>
2022-09-08 21:16:30 +0530

    Time: 7 ms (98.98%), Space: 43.1 MB (90.15%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -4,7 +4,7 @@ class Solution {
     }
     
     public boolean checkRest(int start, int end, String s, boolean flag) {
-        while(start < end) {
+        while(start <= end) {
             if(Character.compare(s.charAt(start), s.charAt(end)) != 0) {
                 if(flag) {
                     return checkRest(start+1, end, s, false) || checkRest(start, end-1, s, false);
