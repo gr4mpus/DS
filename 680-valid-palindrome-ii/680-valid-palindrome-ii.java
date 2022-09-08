ce2787250753defd2002a5bc1b67ab24d5ceba58
Rahul Prasad <rahul.prsd.in@gmail.com>
2022-09-08 21:14:27 +0530

    Time: 8 ms (95.66%), Space: 54.9 MB (25.14%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,31 +1,20 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int start = 0;
-        int end = s.length()-1;
-        boolean flag = true;
+        return checkRest(0, s.length()-1, s, true);
+    }
+    
+    public boolean checkRest(int start, int end, String s, boolean flag) {
         while(start <= end) {
-            if(Character.compare(s.charAt(start), s.charAt(end)) == 0) {
-                start++;
-                end--;
-            } else {
+            if(Character.compare(s.charAt(start), s.charAt(end)) != 0) {
                 if(flag) {
-                    if(checkRest(start+1, end, s) || checkRest(start, end-1, s)) {
-                        return true;
-                    } else {
-                        return false;
-                    }
+                     if(checkRest(start+1, end, s, false) || checkRest(start, end-1, s, false)) {
+                         return true;
+                     } else {
+                         return false;
+                     }
                 } else {
                     return false;
                 }
-            }
-        }
-        return true;
-    }
-    
-    public boolean checkRest(int start, int end, String s) {
-        while(start <= end) {
-            if(Character.compare(s.charAt(start), s.charAt(end)) != 0) {
-                return false;
             }
             start++;
             end--;
