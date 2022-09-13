5954a3b432d0bd8c913da60bf309cb99c49869c6
Rahul Prasad <rahul.prsd.in@gmail.com>
2022-09-13 18:16:05 +0530

    Time: 1381 ms (5.12%), Space: 83 MB (5.00%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int getPrecedence(char ch) {
+        if(ch == '-' || ch == '+') {
+            return 1;
+        }else if (ch == '*' || ch == '/') {
+            return 2;
+        }
+        return 0;
+    }
+    public void calculateTopTwo (Stack<Integer> operand, Stack<Character> operator) {
+        int val2 = operand.pop();
+        int val1 = operand.pop();
+        char op = operator.pop();
+        int res = 0;
+        if(op == '-') {
+            res = val1-val2;
+        } else if (op == '+') {
+            res = val1+val2;
+        } else if (op == '*') {
+            res = val1*val2;
+        } else if (op == '/') {
+            res = val1/val2;
+        }
+        operand.push(res);
+        
+    }
+    public int calculate(String s) {
+        String str = s.trim();
+        int strLen = str.length();
+        int index = 0;
+        Stack<Integer> operand = new Stack<>();
+        Stack<Character> operator = new Stack<>();
+        while(index < strLen) {
+            if(Character.isDigit(str.charAt(index))) {
+                String num = "";
+                while(index<strLen && Character.isDigit(str.charAt(index))) {
+                    System.out.println("num"+num);
+                    num = num + str.charAt(index);
+                    index++;
+                }
+                if(num.length() > 0) {
+                    operand.push(Integer.parseInt(num));
+                }
+            }
+            
+            if(index<strLen && str.charAt(index) == ' '){
+                index++;
+                continue;
+            }
+            System.out.println("Operand"+ operand);
+            System.out.println("operator"+ operator);
+            if(index<strLen && (str.charAt(index) == '-' || str.charAt(index) == '+' || str.charAt(index) == '*' || str.charAt(index) == '/')) {
+                if(!operator.empty() && getPrecedence(operator.peek()) >= getPrecedence(str.charAt(index))) {
+                    while(!operator.empty() && getPrecedence(operator.peek()) >= getPrecedence(str.charAt(index))) {
+                        calculateTopTwo(operand, operator);
+                    }
+                }
+                operator.push(str.charAt(index));
+            }
+            index++;
+        }
+        while(!operator.empty()) {
+            calculateTopTwo(operand, operator);
+        }
+        return operand.peek();
+    }
+}
