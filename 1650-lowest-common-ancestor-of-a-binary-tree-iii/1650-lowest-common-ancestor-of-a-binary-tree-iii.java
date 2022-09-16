dfe787d1cc4b85b9d791e6e335845ed1cdb519e7
Rahul Prasad <rahul.prsd.in@gmail.com>
2022-09-16 16:38:43 +0530

    Time: 93 ms (5.09%), Space: 50.4 MB (6.48%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -10,15 +10,28 @@ class Node {
 
 class Solution {
     public Node lowestCommonAncestor(Node p, Node q) {
-        Queue<Node> queue = new LinkedList<>();
-        getPath(p, queue);
-        while(queue.size() > 0){
-            Node node = queue.remove();
-            if(getParentPath(q, node)) {
-                return node;
-            }
+//         Queue<Node> queue = new LinkedList<>();
+//         getPath(p, queue);
+        
+//         while(queue.size() > 0){
+//             Node node = queue.remove();
+//             if(getParentPath(q, node)) {
+//                 return node;
+//             }
+//         }
+//         return null;
+        HashSet<Integer> set = new HashSet<>();
+        getHashParentPath(p, set);
+        System.out.println(set);
+        return getHashRes(q, set);
+    }
+    
+    public void getHashParentPath(Node p,HashSet<Integer> set) {
+        if(p == null) {
+            return;
         }
-        return null;
+        set.add(p.val);
+        getHashParentPath(p.parent, set);
     }
     
     public void getPath(Node p, Queue<Node> queue) {
@@ -39,5 +52,15 @@ class Solution {
         boolean left = getParentPath(q, parent.left);
         boolean right = getParentPath(q, parent.right);
         return left || right;
+    }
+    
+    public Node getHashRes(Node q, HashSet<Integer> set) {
+        // if(q == null) {
+        //     return false;
+        // }
+        if(set.contains(q.val)) {
+            return q;
+        }
+        return getHashRes(q.parent, set);
     }
 }
